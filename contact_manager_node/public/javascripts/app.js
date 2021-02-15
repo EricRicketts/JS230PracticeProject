f085073cd1fbd43065dfd9088065ad1e319a6f95
EricRicketts <eric_ricketts@me.com>
2021-02-15 09:58:10 -0500

    final cosmetic changes before working on edit form

--- contact_manager_node/public/javascripts/app.js
+++ contact_manager_node/public/javascripts/app.js
@@ -1,4 +1,9 @@
 let App = {
+  editButtonListener: function(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    
+  },
   registerAllPartials: function() {
     Handlebars.registerPartial('show_no_contacts', this.showNoContactsPartial.innerHTML);
     Handlebars.registerPartial('show_all_contacts', this.showAllContactsPartial.innerHTML);
@@ -13,6 +18,16 @@ let App = {
     xhr.addEventListener('load', event => {
       let data = { contacts: event.target.response };
       this.contactsOrFormsDiv.insertAdjacentHTML('afterbegin', this.contactsDisplayTemplate(data));
+      if (data.contacts.length > 0) {
+        let allEditButtons = this.document.getElementsByClassName('edit_button');
+        let allDeleteButtons = this.document.getElementsByClassName('delete_button');
+        Array.from(allEditButtons).forEach(editButton => {
+          editButton.addEventListener('click', this.editButtonListener);
+        });
+        Array.from(allDeleteButtons).forEach(editButton => {
+          editButton.addEventListener('click', this.deleteButtonListener);
+        });
+      }
     });
   },
   init: function(document) {

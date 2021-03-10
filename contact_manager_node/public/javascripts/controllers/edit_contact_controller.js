328c406ff77a5babb4048d00cce5880bdb8124d7
EricRicketts <eric_ricketts@me.com>
2021-03-09 19:19:20 -0500

    edit form is now being successully broght up

--- contact_manager_node/public/javascripts/controllers/edit_contact_controller.js
+++ contact_manager_node/public/javascripts/controllers/edit_contact_controller.js
@@ -10,8 +10,17 @@ let EditContactController = {
     getSingleContactXhr.addEventListener('load', event => {
       let contactData = event.target.response;
       let contactObject = this.app.model.formContactObject(contactData);
+      let contactKeys = Object.keys(contactObject).filter(key => key !== 'tags');
       let tagData = this.app.model.formattedTagsForEditContact(contactObject);
       this.app.view.showEditContactFormAndHeader(headerData, tagData);
+      let form = this.app.document.getElementById('edit_add_contact_form');
+      let personalFormInputs = Array.from(form.getElementsByTagName('input')).filter(input => {
+        return input.id !== 'new_tag';
+      });
+      personalFormInputs.forEach(input => {
+        let contactKey = contactKeys.find(key => key === input.id);
+        input.value = contactObject[contactKey];
+      });
     });
   },
   getSingleContactInformation: function(targetElement) {
